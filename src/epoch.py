Fixed perplexity calculation and untied from Pondering Cost

src/epoch.py
@@ -9,10 +9,11 @@ def run_epoch(session, m, data, eval_op, verbose=False):
     start_time = time.time()
     costs = 0.0
     iters = 0
+    perps = 0
     num_batch_steps_completed = 0
 
     for step, (x, y) in enumerate(reader.ptb_iterator(data, m.batch_size, m.num_steps)):
-        cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+        cost, state, perp, _ = session.run([m.cost, m.final_state, m.perplexity, eval_op],
                                      {m.input_data: x,
                                       m.targets: y})
 
@@ -21,13 +22,14 @@ def run_epoch(session, m, data, eval_op, verbose=False):
                 'you successfully completed one entire batch -- cost', cost, 'time is', time.ctime(),
                 'num_batch_steps_completed:', num_batch_steps_completed)
 
+        perps += perp
         costs += cost
         iters += m.num_steps
         num_batch_steps_completed += 1
 
         if verbose and step % (epoch_size // 10) == 10:
             print("%.3f perplexity: %.3f speed: %.0f wps" %
-                  (step * 1.0 / epoch_size, np.exp(costs / iters),
+                  (step * 1.0 / epoch_size, perps/step ,
                    iters * m.batch_size / (time.time() - start_time)))
 
-    return (costs / iters)
+    return (costs/iters)

src/models/AdaptiveComputationTime.py
@@ -53,13 +53,16 @@ class ACTModel(object):
         softmax_b = tf.get_variable("softmax_b", [self.vocab_size])
         self.logits = tf.matmul(output, softmax_w) + softmax_b  # dim (400, 10,000)(numsteps*batchsize, vocabsize)
 
-        loss = seq2seq.sequence_loss_by_example(
+        # by default averages across time steps
+        loss = seq2seq.sequence_loss(
             [self.logits],
             [tf.reshape(self.targets, [-1])],
             [tf.ones([batch_size * num_steps])])
 
+        loss_batch = tf.reduce_sum(loss)
+        self.perplexity = tf.exp(loss_batch)
         # add up loss and retrieve batch-normalised ponder cost: sum N + sum Remainder
-        self.cost = tf.reduce_sum(loss) / batch_size + \
+        self.cost = loss_batch + \
                     self.act.CalculatePonderCost(time_penalty=self.config.ponder_time_penalty)
 
         self.final_state = self.outputs[-1]